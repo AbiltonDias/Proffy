1c1f0719b1f17d38ce3086b1bb2686ca80936e1e
Abilton Dias <abiltongd@gmail.com>
2020-08-07 00:03:08 -0300

    Component Header implementado

--- src/pages/TeachersList/index.tsx
+++ src/pages/TeachersList/index.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import PageHeader from '../../components/PageHearder';
 
-import backIcon from '../../assets/images/logo.svg';
-import logoImg from '../../assets/images/icons/back.svg';
+
 import './styles.css';
 
 const TeachersList = () => {
     return(
         <div id="page-teacher-list" className='container'>
-            <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to='/'>
-                        <img src={backIcon} alt='Voltar'/>
-                    </Link>
-                    <img src={logoImg} alt="Proffy"/>
-                </div>
-                <div className="header-content">
-                    <strong> Esses são os Proffs disponiveis </strong>
-                </div>
-            </header>
+           <PageHeader />
         </div>
     )
 };
